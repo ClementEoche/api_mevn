2b77e02bc4d51a64d9e1ff73f26bf36330f5d934
ClementEoche <clement.eoche35@gmail.com>
2020-11-30 20:36:20 +0100

    nice

--- app/routes/task.routes.js
+++ app/routes/task.routes.js
@@ -1,28 +1,37 @@
 module.exports = app => {
     const tasks = require("../controllers/task.controller.js");
+    const { authJwt } = require("../middlewares");
 
     var router = require("express").Router();
 
     // Create a new Tutorial
-    router.post("/", tasks.create);
+    router.post("/",[authJwt.verifyToken] ,tasks.create);
 
     // Retrieve all Tutorials
-    router.get("/", tasks.findAll);
+    router.get("/",[authJwt.verifyToken], tasks.findAll);
 
     // Retrieve all published Tutorials
-    router.get("/done", tasks.findAllDone);
+    router.get("/done",[authJwt.verifyToken], tasks.findAllDone);
 
     // Retrieve a single Tutorial with id
-    router.get("/:id", tasks.findOne);
+    router.get("/:id",[authJwt.verifyToken], tasks.findOne);
 
     // Update a Tutorial with id
-    router.put("/:id", tasks.update);
+    router.put("/:id",[authJwt.verifyToken], tasks.update);
 
     // Delete a Tutorial with id
-    router.delete("/:id", tasks.delete);
+    router.delete("/:id",[authJwt.verifyToken], tasks.delete);
 
     // Create a new Tutorial
-    router.delete("/", tasks.deleteAll);
+    router.delete("/",[authJwt.verifyToken], tasks.deleteAll);
+
+    app.use(function(req, res, next) {
+        res.header(
+          "Access-Control-Allow-Headers",
+          "x-access-token, Origin, Content-Type, Accept"
+        );
+        next();
+    });
 
     app.use("/api/tasks", router);
 };
